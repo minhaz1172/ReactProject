3c914f7671dab19c164e3939aed6362144e85fa0
Minhaz Uddin <uminhaz885@gmail.com>
2024-08-01 23:47:03 +0600

    Foodlist added by Bootstrap List

---
+++ HealhtyFood/foodlist1.jsx
@@ -0,0 +1,16 @@
+const FoodList = () => {
+  return (
+    <>
+      <h1>healthy Food</h1>
+      <ul class="list-group">
+        <li class="list-group-item">Chal</li>
+        <li class="list-group-item">Dal</li>
+        <li class="list-group-item">Fish</li>
+        <li class="list-group-item">meat</li>
+
+      </ul>
+    </>
+  )
+
+}
+export default FoodList

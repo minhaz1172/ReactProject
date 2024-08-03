98dd1cc60d649cb2385a295194acb95b7c1348cb
Minhaz Uddin <uminhaz885@gmail.com>
2024-08-03 19:55:57 +0600

    Parameter passing via props

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Prop1 from './prop_parameter'
+import './Prop.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='container'>
+        <Prop1 title="FontEnd Basics" description="This is the list you should learn to be a frontend devloper" /> {/*passing multiple parameter to the child jsx file via props*/}
+      </div>
+
+
+
+    </>
+  )
+}
+
+export default App

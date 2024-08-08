cc77b495a92913b975f3d6bd012b137a07c0fd8a
Minhaz Uddin <uminhaz885@gmail.com>
2024-08-08 21:12:24 +0600

    handled  event by  onchange event

--- Handling Event,States and Passing Children/React State,Event And passing children/src/Foodinput.jsx
+++ Handling Event,States and Passing Children/React State,Event And passing children/src/Foodinput.jsx
@@ -1,11 +1,15 @@
 import styles from './Foodinput.module.css';
 
 const Foodinput = () => {
+  const onChange_event = (event) => {
+    console.log(event);
+  }
+
   return (
     <>
       <h1 className="text-center">Healthy Food</h1>
       <input type="text" placeholder="Enter Text here" className={styles.Foodinput}
-        onChange={(event) => console.log(event)}
+        onChange={onChange_event}
       />
 
     </>

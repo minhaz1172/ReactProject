9a7cd9b31bad6f1f63bbf6d6482223235c0fac4a
Minhaz Uddin <uminhaz885@gmail.com>
2024-08-08 21:15:25 +0600

    target value is handlled by event.target.value

--- Handling Event,States and Passing Children/React State,Event And passing children/src/Foodinput.jsx
+++ Handling Event,States and Passing Children/React State,Event And passing children/src/Foodinput.jsx
@@ -2,14 +2,14 @@ import styles from './Foodinput.module.css';
 
 const Foodinput = () => {
   const onChange_event = (event) => {
-    console.log(event);
+    console.log(event.target.value);
   }
 
   return (
     <>
       <h1 className="text-center">Healthy Food</h1>
       <input type="text" placeholder="Enter Text here" className={styles.Foodinput}
-        onChange={onChange_event}
+        onChange={onChange_event}/*function called*/
       />
 
     </>

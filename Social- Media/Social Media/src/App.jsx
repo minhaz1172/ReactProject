9f1577cfba691accb1bac9f88179bde0bd55ec4f
Minhaz Uddin <uminhaz885@gmail.com>
2024-08-14 01:32:24 +0600

    onclick action added when sidebar option click it work

--- Social- Media/Social Media/src/App.jsx
+++ Social- Media/Social Media/src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './Components/Header'
-import Footer from './Components/footer'
-import Sidebar from './Components/Sidebar'
-
-import Postlist from './Components/postlist'
-import CreatePost from './Components/Create Post'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/footer';
+import Sidebar from './Components/Sidebar';
+import Postlist from './Components/postlist';
+import CreatePost from './Components/Create Post';
 
 function App() {
-  const [selecedTab, setSelectedTab] = useState("Home");
+  const [selectedTab, setSelectedTab] = useState("");
+
   let content;
-  if (selecedTab === 'Home') {
-    content = <Postlist />
-  }
-  else {
-    content = <CreatePost />
+  if (selectedTab === 'Home') {
+    content = <Postlist />;
+  } else {
+    content = <CreatePost />;
   }
 
   return (
     <div className='app-container'>
-      <Sidebar />
+      <Sidebar select={selectedTab} setSelectedTab={setSelectedTab} />
       <div className='components'>
         <Header />
         {content}
         <Footer />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Social- Media/Social Media/src/Components/Sidebar.jsx
+++ Social- Media/Social Media/src/Components/Sidebar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-function Sidebar() {
+function Sidebar(props) {
+
+
   return (
     <>
       <div
@@ -19,7 +21,12 @@ function Sidebar() {
         <hr />
         <ul className="nav nav-pills flex-column mb-auto">
           <li className="nav-item">
-            <a href="#" className="nav-link active" aria-current="page">
+            <a
+              href="#"
+              className={`nav-link text-white ${(props.select) === "Home" ? "active" : ""}`}
+              aria-current="page"
+              onClick={() => props.setSelectedTab("Home")}  // * this will set selectedTab value equal to Home automatically
+            >
               <svg className="bi pe-none me-2" width="16" height="16">
                 <use xlinkHref="#home"></use>
               </svg>
@@ -27,63 +34,18 @@ function Sidebar() {
             </a>
           </li>
           <li>
-            <a href="#" className="nav-link text-white">
+            <a
+              href="#"
+              className={`nav-link text-white ${(props.select) === "Create Post" ? "active" : ""}`}
+              onClick={() => props.setSelectedTab("Create Post")} // * this will set selectedTab value equal to Create Post Automatically
+            >
               <svg className="bi pe-none me-2" width="16" height="16">
                 <use xlinkHref="#speedometer2"></use>
               </svg>
               Create Post
             </a>
           </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              <svg className="bi pe-none me-2" width="16" height="16">
-                <use xlinkHref="#table"></use>
-              </svg>
-              Orders
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              <svg className="bi pe-none me-2" width="16" height="16">
-                <use xlinkHref="#grid"></use>
-              </svg>
-              Products
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              <svg className="bi pe-none me-2" width="16" height="16">
-                <use xlinkHref="#people-circle"></use>
-              </svg>
-              Customers
-            </a>
-          </li>
         </ul>
-        <hr />
-        <div className="dropdown">
-          <a
-            href="#"
-            className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <img
-              src="https://github.com/mdo.png"
-              alt=""
-              width="32"
-              height="32"
-              className="rounded-circle me-2"
-            />
-            <strong>mdo</strong>
-          </a>
-          <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
-            <li><a className="dropdown-item" href="#">New project...</a></li>
-            <li><a className="dropdown-item" href="#">Settings</a></li>
-            <li><a className="dropdown-item" href="#">Profile</a></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="#">Sign out</a></li>
-          </ul>
-        </div>
       </div>
     </>
   );

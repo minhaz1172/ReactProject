d3d3b33fa9bf98af74c0a247b09c6eda0d0dcb79
Minhaz Uddin <uminhaz885@gmail.com>
2024-08-16 18:23:29 +0600

    createpost.jsx have been edited for post

--- Social- Media/Social Media/src/Components/Create Post.jsx
+++ Social- Media/Social Media/src/Components/Create Post.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 function CreatePost() {
   return (
-    <form>
+    <form className='form-content'>
       <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        <label htmlFor="title" className="form-label">Post Title</label>
+        <input type="text" className="form-control" id="title" placeholder='what are you feeling Today' />
+
       </div>
       <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-        <input type="password" className="form-control" id="exampleInputPassword1" />
-      </div>
-      <div className="mb-3 form-check">
-        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+        <label htmlFor="body" className="form-label">Post Content</label>
+
+        <div class="">
+          <textarea class="form-control" placeholder="Describe Your Feelings" id="floatingTextarea" rows="4"></textarea> {/*here row=4 will bigger the size of textarea */}
+
+        </div>
       </div>
       <button type="submit" className="btn btn-primary">Submit</button>
     </form>
